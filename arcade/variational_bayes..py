102e5b71f8b8220f07cb8286629be4ef67b21f15
Ftoy <spit10000@yahoo.gr>
2024-12-06 11:42:45 +0200

    vae sample

--- arcade/variational_bayes..py
+++ arcade/variational_bayes..py
@@ -30,9 +30,6 @@ def train(epochs=10):
     train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 
-    test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
-
     # Model, optimizer
     input_dim = 28 * 28  # 28x28 images flattened
     hidden_dim = 400  # Hidden layer size
@@ -67,26 +64,45 @@ def sample_from_vae(vae, num_samples=1, latent_dim=20):
     # Sample from a standard normal distribution
     z = torch.randn(num_samples, latent_dim)
 
-    # Pass the sampled latent vectors through the decoder
     with torch.no_grad():
         x_reconstructed = vae.decoder(z)
 
     return x_reconstructed
 
 
-if __name__ == '__main__':
-    vae = train(epochs=10)
+def sample_from_reconstruct(vae):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.view(-1))  # Flatten the images
+    ])
+    test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+    # Evaluate on test data
+    vae.eval()  # Set to evaluation mode
+    with torch.no_grad():
+        test_images, _ = next(iter(test_loader))
+        test_images = test_images.view(test_images.size(0), -1)
+        reconstructed = vae(test_images)
 
-    # Example usage
-    num_samples = 10  # How many samples you want to generate
-    latent_dim = 20  # The dimension of the latent space
-    generated_samples = sample_from_vae(vae, 10, 20)
+    # Visualize original and reconstructed images
+    test_images = test_images.view(-1, 28, 28)
+    reconstructed = reconstructed.view(-1, 28, 28)
 
-    # Assuming the samples are image tensors, you may want to visualize them
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # Original images
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(test_images[i].cpu().numpy(), cmap="gray")
+        plt.axis("off")
 
-    # Visualize the generated images
-    for i in range(num_samples):
-        plt.subplot(1, num_samples, i + 1)
-        plt.imshow(generated_samples[i].view(28, 28).cpu().numpy(), cmap='gray')
-        plt.axis('off')
+        # Reconstructed images
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(reconstructed[i].cpu().numpy(), cmap="gray")
+        plt.axis("off")
     plt.show()
+
+
+if __name__ == '__main__':
+    vae = train(epochs=3)
+    sample_from_reconstruct(vae)
