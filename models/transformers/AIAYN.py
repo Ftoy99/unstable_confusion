89781b312c355dfd4d7afc81cc46663cc5abe674
Ftoy <spit10000@yahoo.gr>
2024-12-09 16:00:15 +0200

    Classes

--- models/transformers/AIAYN.py
+++ models/transformers/AIAYN.py
@@ -3,10 +3,23 @@ from torch import nn
 
 
 class AIAYN(nn.Module):
-
+    """Transformer class as described in Attention is all you need"""
     def __init__(self):
         super(AIAYN, self).__init__()
-        self.test: nn.Linear = nn.Linear(1, 1)
+        self.encoder = Encoder()
+        self.decoder = Decoder()
 
     def forward(self, x):
-        return self.test(x)
+        return self.decoder(self.encoder(x))
+
+
+class Encoder(nn.Module):
+    """Encoder class for the AIAYN Transformer"""
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+
+class Decoder(nn.Module):
+    """Decoder class for the AIAYN Transformer"""
+    def __init__(self):
+        super(Decoder, self).__init__()
